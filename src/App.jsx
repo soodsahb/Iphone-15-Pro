918edc6eae2d79bfdf67c69e18648c5008738708
Daddy <daddy123@gmail.com>
2024-03-30 20:24:13 +0530

    check it out

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Chip from "./components/Chip"
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Model from "./components/Model"
+import Navbar from "./components/Navbar"
+
+import * as Sentry from '@sentry/react'
+
+function App() {
+ 
+ 
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+      <Chip/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Sentry.withProfiler(App)
